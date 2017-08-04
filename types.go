842c8fd17d71a946d0fb68133efb4d2ad7e9aa4e
Tomasz Jurkiewicz <tomasz.jurkiewicz@polidea.com>
2017-08-04 10:49:37 +0200

    Added api features

--- types.go
+++ types.go
@@ -12,18 +12,21 @@ type ErrorResponse struct {
 
 type ListingResponse struct {
   Listing struct {
-    Id             int64     `json:"id"`
-    City           string    `json:"city"`
-    UserId         int64     `json:"user_id"`
-    Latitude       float64   `json:"lat"`
-    Longitude      float64   `json:"lng"`
-    Bathrooms      float64   `json:"bathrooms"`
-    Bedrooms       float64   `json:"bedrooms"`
-    Beds           float64   `json:"beds"`
-    PersonCapacity int       `json:"person_capacity"`
-    CountryCode    string    `json:"country_code"`
-    Amenities      []string  `json:"amenities"`
+    Id                    int64     `json:"id"`
+    City                  string    `json:"city"`
+    UserId                int64     `json:"user_id"`
+    Latitude              float64   `json:"lat"`
+    Longitude             float64   `json:"lng"`
+    Bathrooms             float64   `json:"bathrooms"`
+    Bedrooms              float64   `json:"bedrooms"`
+    Beds                  float64   `json:"beds"`
+    PersonCapacity        int       `json:"person_capacity"`
+    CountryCode           string    `json:"country_code"`
+    Amenities             []string  `json:"amenities"`
+    AmenitiesIds          []int64   `json:"amenities_ids"`
+    IsLocationExact       bool      `json:"is_location_exact"`
+    InBuilding            bool      `json:"in_building"`
+    InLandlordPartnership bool      `json:"in_toto_area"`
   } `json:"listing"`
 }
-
 
